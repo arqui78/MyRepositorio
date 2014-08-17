5b31a648069ffeb180db36641618a88aea3ed261
arqui78 <arqui78@gmail.com>
2014-08-17 11:36:02 -0430

    Clase Hola GitHub

---
+++ hola_github.java
@@ -0,0 +1,17 @@
+
+/**
+* @author Arquimedes Rodriguez
+**/
+
+public Class Hola_GitHub{
+	private String mensaje;
+
+	public Hola_GitHub(){
+		this.mensaje = "Hola GitHub";
+		saludar();
+	}
+	private saludar(){
+		System.out.println(this.mensaje);
+
+	}
+}
